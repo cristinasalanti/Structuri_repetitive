f15663242549b92c8cd2ad24c9c09473177753b9
cristina <salanticristina1297@gmail.com>
2019-04-23 16:49:52 +0300

    Exercitiul doi - tema structuri repetitive

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void Main(String[] args) {
+
+        int n=11;
+        if (n % 2 == 0) System.out.println(n / 2);
+        else if (n % 2 != 0) System.out.println(( 3*n)+1);
+
+    }
+
+}
