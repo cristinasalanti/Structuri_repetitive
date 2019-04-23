e9f689fc13b1d30a423a87cc4ebe0a747e74a4d5
cristina <salanticristina1297@gmail.com>
2019-04-23 18:13:26 +0300

    Exercitiul sase - tema structuri repetitive

--- src/main.java
+++ src/main.java
@@ -1,12 +1,12 @@
 public class main {
     public static void main(String[] args) {
 
-        int nr=5;
-        while(nr!=1) {
-            nr--;
+        int n=11;
+        for(int i=0;i<=n;i++)
+        {
 
-
-            System.out.println(nr);
+            if(i%2==1)
+            System.out.println(i);
         }
 
 
