82bca074eb91f1304605cf45614133a5368224b9
cristina <salanticristina1297@gmail.com>
2019-04-23 17:27:39 +0300

    Exercitiul patru - tema structuri repetitive

--- src/main.java
+++ src/main.java
@@ -1,15 +1,18 @@
 public class main {
     public static void main(String[] args) {
 
-        int n = 6;
-        int counter = 1;
-           while(n!=1) {
-               if (n % 2 == 0) {
-                   n = n / 2;}
-               else if (n % 2 != 0) n = (3 * n) + 1;
-               counter++;
-           }
-        System.out.println(counter);
+       int[] n=new int[]{2, 6, 7,5,6,34,2,4,7,7,3,1,2,5,6,8,4,2};
+       int nrpare  = 0;
+        int nrimpare = 0;
+        for(int i=0; i<n.length; i++)
+        {
+            if(n[i]%2 == 0) {nrpare++;}
+            else if(n[i]%2 != 0) {nrimpare++;}
+
+        }
+        System.out.println(nrpare);
+        System.out.println(nrimpare);
+
     }
 
 }
