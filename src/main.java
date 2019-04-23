a145e29ffacb89bc5394d95a040ac53db8fb2004
cristina <salanticristina1297@gmail.com>
2019-04-23 18:39:55 +0300

    Exercitiul sapte - tema structuri repetitive

--- src/main.java
+++ src/main.java
@@ -1,14 +1,27 @@
+
+// 2*(3^n)
 public class main {
     public static void main(String[] args) {
 
-        int n=11;
-        for(int i=0;i<=n;i++)
+        int n = 7;
+        int suma = 0;
+        int pow = 1;
+        int inmultire = 1;
+        for (int i = 0; i < n; i++)
         {
+            pow = 3*pow;
 
-            if(i%2==1)
-            System.out.println(i);
+            inmultire = 2 * (pow);
+
         }
 
+        System.out.println(inmultire);
 
+        while(inmultire!=0)
+        {
+            suma = suma + inmultire % 10;
+           inmultire= inmultire / 10;
+        }
+        System.out.println(suma);
     }
-}
+    }
