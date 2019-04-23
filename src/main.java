8302018a398e147c6fc390e5289e8c1db459208b
cristina <salanticristina1297@gmail.com>
2019-04-23 19:46:24 +0300

    Exercitiul zece - tema structuri repetitive

--- src/main.java
+++ src/main.java
@@ -1,16 +1,21 @@
 public class main {
     public static void main(String[] args) {
 
-        int n=6;
-        for (int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=i;j++)
+        int[] sir= new int[]{2,3,7,8,6,2,10,3,7,4,6,9,10};
+        int cnt=0;
+        int suma=0;
+            for(int i=0;i<sir.length;i++)
             {
-                System.out.printf(j+ " ");
-
+                if(sir[i]%2==0)
+                {
+                    cnt++;
+                    if(cnt<=5)
+                    {
+                        suma = suma + sir[i];
+                    }
+                }
             }
-            System.out.println();
-        }
+            System.out.println(suma);
 
     }
     }
