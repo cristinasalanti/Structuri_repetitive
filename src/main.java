ce8300cf8d233b5342c0b82d3b328ef08dace585
cristina <salanticristina1297@gmail.com>
2019-04-23 17:52:56 +0300

    Exercitiul trei - tema structuri repetitive

--- src/main.java
+++ src/main.java
@@ -1,18 +1,21 @@
 public class main {
     public static void main(String[] args) {
 
-       int[] n=new int[]{2, 6, 7,5,6,34,2,4,7,7,3,1,2,5,6,8,4,2};
-       int nrpare  = 0;
-        int nrimpare = 0;
-        for(int i=0; i<n.length; i++)
-        {
-            if(n[i]%2 == 0) {nrpare++;}
-            else if(n[i]%2 != 0) {nrimpare++;}
+        int[] n = new int[]{2,4,6,8,9};
+        int max = 0;
+        for (int i = 0; i < n.length; i++) {
 
+            if(n[i]%2 !=0)
+            {
+
+                if(max<n[i]) max = n[i];
+
+            }
         }
-        System.out.println(nrpare);
-        System.out.println(nrimpare);
+        if(max==0) System.out.println("NO");
+         else System.out.println(max);
+
+
 
     }
-
 }
