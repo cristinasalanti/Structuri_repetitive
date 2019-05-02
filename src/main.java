c2b2546c087277082286e99999dff51bd440cebf
cristina <salanticristina1297@gmail.com>
2019-05-02 15:11:52 +0300

    Exercitiul 15 - tema structuri repetitive

--- src/main.java
+++ src/main.java
@@ -1,15 +1,24 @@
-    public class main {
-        public static void main(String[] args) {
+        public class main {
+            public static void main(String[] args) {
 
-            int n=6;
+             int n=90;
+             int m=9;
+             int numar=0;
+               while(n!=0) {
 
-            while(n>0 && n<7)
-            {
-                n--;
-                System.out.println("going");
+                  numar =numar *10+n%10;
+                  n=n/10;
 
+
+                }
+                if(numar == m)
+                {
+                    System.out.println("true");
+                }
+                else if (numar !=  m) System.out.println("false");
+        // nu am reusit sa verific conditiile pentru numere cu 01,02,03...09
 
             }
 
-        }
-    }
+            }
+
