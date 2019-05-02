5c490d5af326a04c6b45894cd4f8791fc5add8d1
cristina <salanticristina1297@gmail.com>
2019-05-02 16:25:42 +0300

    Exercitiul 17 - tema structuri repetitive

--- src/main.java
+++ src/main.java
@@ -1,22 +1,31 @@
         public class main {
             public static void main(String[] args) {
 
-            int n=12345678;
-            int x;
-            int i=-1;
-            int suma=0;
+            int n=1234567891;
+            int m;
+           int i=-1;
+            int sumapare=0;
+            int sumaimpare=0;
+            int x=0;
            while(n!=0) {
+
                     x = n%10;
                     i++;
                     if(i%2 ==0 )
-                        suma = suma+x;
+                        sumapare = sumapare+x;
+                    else sumaimpare = sumaimpare + x;
 
                     n = n/10;
-               System.out.println(suma);
+
 
 
            }
+
+                m = sumapare-sumaimpare;
+                System.out.println(m);
+
+                }
             }
 
-            }
+            
 
