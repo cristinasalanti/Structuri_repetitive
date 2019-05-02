09d412fbc8b5ea2bb68de38193d3a8ef4ad7d342
cristina <salanticristina1297@gmail.com>
2019-05-02 14:16:43 +0300

    Exercitiul 12 - tema structuri repetitive- cu if - else

--- src/main.java
+++ src/main.java
@@ -1,21 +1,24 @@
     public class main {
         public static void main(String[] args) {
 
-            int n =15;
+            int n =11;
 
-            if (n == 0) System.out.println("zero");
+            if (n == 0) System.out.println("ianuarie");
 
-                else if (n == 1) System.out.println("unu");
-                else if (n == 2) System.out.println("doi");
-               else if (n == 3) System.out.println("trei");
-                else if(n == 4) System.out.println("patru");
-                else if(n == 5) System.out.println("cinci");
-                 else if (n == 6) System.out.println("sase");
-                 else if (n == 7) System.out.println("sapte");
-                 else if (n == 8) System.out.println("opt");
-                 else if(n == 9) System.out.println("noua");
+                else if (n == 1) System.out.println("februarie");
+                else if (n == 2) System.out.println("martie");
+               else if (n == 3) System.out.println("aprilie");
+                else if(n == 4) System.out.println("mai");
+                else if(n == 5) System.out.println("iunie");
+                 else if (n == 6) System.out.println("iulie");
+                 else if (n == 7) System.out.println("august");
+                 else if (n == 8) System.out.println("septembrie");
+                 else if(n == 9) System.out.println("octombrie");
+            else if(n == 10) System.out.println("noiembrie");
+            else if(n == 11) System.out.println("decembrie");
 
-            else System.out.println("numar gresit");
+
+            else System.out.println("luna gresita");
 
         }
     }
