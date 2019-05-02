198885a41321513e2f1d0e7b2ff0bd8cf2f2aba0
cristina <salanticristina1297@gmail.com>
2019-05-02 14:22:18 +0300

    Exercitiul 13 - tema structuri repetitive

--- src/main.java
+++ src/main.java
@@ -3,38 +3,10 @@
 
             int n =6;
 
-            String luna;
+            for (int i = 0; i < n; i++) {
+                System.out.println("go");
 
-            switch(n) {
-
-                case 0: luna = "ianuarie";
-                    break;
-                case 1:  luna = "februarie";
-                    break;
-                case 2:  luna = "martie";
-                    break;
-                case 3:  luna = "aprilie";
-                    break;
-                case 4:  luna = "mai";
-                    break;
-                case 5:  luna = "iunie";
-                    break;
-                case 6:  luna = "iulie";
-                    break;
-                case 7:  luna = "august";
-                    break;
-                case 8:  luna = "septembrie";
-                    break;
-                case 9:  luna = "octombrie";
-                    break;
-                case 10:  luna = "noiembrie";
-                    break;
-                case 11:  luna = "decembrie";
-                    break;
-                default: luna = "luna gresita";
-                    break;
             }
-            System.out.println(luna);
-
+            System.out.println("gata");
         }
     }
